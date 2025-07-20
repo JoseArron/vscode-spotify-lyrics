c7fcb03210764c4fe533fb4f153c5bb4f4d70737
Jose <joseearronn@gmail.com>
2025-07-21 01:13:57 +0800

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const EXT_INFO = {
+  name: 'Spotify Lyrics',
+  id: 'spotify-lyrics',
+  description: 'View lyrics for the currently playing Spotify track.'
+};
+
+export const COMMANDS = {
+  TEST: 'spotify-lyrics.test'
+};
+
+export const WEBVIEW_VIEW_ID = 'spotify-lyrics.view';
+
+export const MESSAGES = {
+  REQ_AUTH_STATUS: 'request-auth-status',
+  SEND_AUTH_STATUS: 'send-auth-status',
+  REQ_LOG_IN: 'request-log-in',
+  REQ_LOG_OUT: 'request-log-out'
+};
+
+export const BUTTONS = {
+  LOG_IN: 'Log in'
+};
